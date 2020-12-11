c46cfb9b791255f285fc3503c0f717d39320c5b3
pritesh <mehta.pritesh@gmail.com>
2020-12-11 10:59:16 +0800

    update

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/hello', (req, res) => {
-  res.send('Hello Skrilla!')
+//app.set('domain', '0.0.0.0');
+
+app.get('/', (req, res) => {
+  res.send('This is app1')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('/ping', (req, res) => {
+  res.send('Pong')
+})
+
+app.get('/sendemail', (req, res) => {
+  res.send('sending email')
+})
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Example app listening at http://0.0.0.0:${port}`)
 })
